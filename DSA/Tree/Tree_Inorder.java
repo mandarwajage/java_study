85dbf5f5945c00b24e4e9249f01ed774c9de2f26
mandar wajage <mandarwajage@gmail.com>
2024-08-20 00:20:58 +0530

    added 1st code

---
+++ DSA/Tree/Tree_Inorder.java
@@ -0,0 +1,47 @@
+package tree.dsa;
+
+class Node {
+    int data;
+    Node left, right;
+
+    public Node(int item) {
+        data = item;
+        left = right = null;
+    }
+}
+
+class BinaryTree {
+    Node root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    void inorderTraversal(Node node) {
+        if (node == null)
+            return;
+
+        inorderTraversal(node.left);
+        System.out.print(node.data + " ");
+        inorderTraversal(node.right);
+    }
+
+    void printInOrder() {
+        inorderTraversal(root);
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+
+        System.out.print("In-order traversal of the binary tree: ");
+        tree.printInOrder();
+    }
+}
